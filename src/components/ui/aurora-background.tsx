ad3852a1d90683de5e1eaa467c40400f13de0f5d
Anderson-Soares-Martins <andersoaresmartins@gmail.com>
2024-09-15 19:53:23 -0300

    fix: show background aurora

--- src/components/ui/aurora-background.tsx
+++ src/components/ui/aurora-background.tsx
@@ -22,7 +22,7 @@ export const AuroraBackground = React.memo(
         {...props}
       >
         <div
-          className="absolute inset-0 overflow-hidden"
+          className="hidden md:block absolute inset-0 overflow-hidden"
           style={{
             height: "calc(100vh - 60px)"
           }}
@@ -30,7 +30,6 @@ export const AuroraBackground = React.memo(
           <div
             className={cn(
               `
-              hidden
               [--white-gradient:repeating-linear-gradient(100deg,var(--white)_0%,var(--white)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--white)_16%)]
               [--dark-gradient:repeating-linear-gradient(100deg,var(--black)_0%,var(--black)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--black)_16%)]
               [--aurora:repeating-linear-gradient(100deg,var(--blue-500)_10%,var(--indigo-300)_15%,var(--blue-300)_20%,var(--violet-200)_25%,var(--blue-400)_30%)]
@@ -44,7 +43,7 @@ export const AuroraBackground = React.memo(
               after:[background-size:200%,_100%] 
               after:animate-aurora after:[background-attachment:fixed] after:mix-blend-difference
               pointer-events-none
-              md:absolute -inset-[10px] opacity-50 will-change-transform`,
+              absolute -inset-[10px] opacity-50 will-change-transform`,
               showRadialGradient &&
                 `[mask-image:radial-gradient(ellipse_at_100%_0%,black_10%,var(--transparent)_70%)]`
             )}
